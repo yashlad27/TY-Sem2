ff000f2d8f79e00987fe29306ea25a41c5a29733
Yash Lad <yashlad727@gmail.com>
2023-01-15 22:27:41 +0530

    arrays and methods

--- udemy/ZTM_Interview_Prep/Arrays/index.js
+++ udemy/ZTM_Interview_Prep/Arrays/index.js
@@ -1,10 +1,18 @@
 const strings = ['a', 'b', 'c', 'd'];
 console.log(strings);
 
+console.log(strings[2])
+
 strings.push('e'); // O(1)
 console.log(strings);
 
 strings.pop();
 strings.pop(); // O(1)
+
+// unshift:
+strings.unshift('x'); // o(n)
+
+// splice: add element in the middle
+strings.splice(2, 0, 'alien')
 
 console.log(strings);
