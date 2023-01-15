ec886ee686409c2ffff98c358bd2908d29456102
Yash Lad <yashlad727@gmail.com>
2023-01-15 22:00:56 +0530

    matrix in python

---
+++ udemy/python/python_basics_2/matrix01.py
@@ -0,0 +1,17 @@
+# Matrix: an array of arrays
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+pic_matrix = [
+    [1, 0, 1],
+    [0, 1, 0],
+    [1, 0, 1]
+]
+
+print(matrix[0][1])
+
+print(pic_matrix)
