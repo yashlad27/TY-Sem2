9f76e21cacd760533a5e5f3985f8c4339ff4fe8c
Yash Lad <yashlad727@gmail.com>
2023-01-23 19:23:56 +0530

    mac os commit

---
+++ udemy/ZTM_Interview_Prep/Arrays/classes.js
@@ -0,0 +1,13 @@
+// classes in javascript
+
+// Reference Type, Context, Instantiation
+
+console.log([] === []);
+
+var object1 = {value:10};
+var object2 = object1;
+var object3 = {value:10};
+
+console.log(object1 === object2);
+
+console.log(object1 === object3);
