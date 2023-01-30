265fc9626134fc8b91992114e87b15502689909c
Yash Lad <yashlad727@gmail.com>
2023-01-30 22:48:39 +0530

    win os commit dictionaries and none is python

---
+++ udemy/python/python_basics_2/dict.py
@@ -0,0 +1,23 @@
+# Dictionary:
+
+dictionary = {
+    'a': 1,
+    'b': 2
+}
+
+print(dictionary['b'])
+
+new_list = [
+    {
+    'a': [1, 2, 3],
+    'b': "hello",
+    'x': True
+    },
+    {
+    'a': [4, 5, 6],
+    'b': "bye",
+    'x': True  
+    }
+]
+
+print(new_list[0]['a'])
---
+++ udemy/python/python_basics_2/none_use.py
@@ -0,0 +1,3 @@
+
+weapons = None
+print(weapons)
